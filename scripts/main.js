4d3c38515853209e24db86fc5ca385d93526f920
Jessica Sturgell <sturgell01@gmail.com>
2024-05-09 10:30:09 -0400

    added basic html

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+const container = document.querySelector("#container")
+
+const composedHTML = `
+<h1>Cars-R-Us</h1>
+<article class="choices">
+    <section class="choices_color options">
+        <h2>Color</h2>
+    </section>
+    <section class="choices_interior options">
+        <h2>Interior</h2>
+    </section>
+    <section class="choices_technology options">
+        <h2>Technology</h2>
+    </section>
+    <section class="choices_wheel options">
+        <h2>Wheels</h2>
+    </section>
+</article>
+`
+container.innerHTML = composedHTML
